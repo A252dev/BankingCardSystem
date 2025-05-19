7fdfdf3425a495097b9d84cbdc4487feb51d9c41
A252dev <a252p@outlook.com>
2025-05-19 13:46:08 +0300

    syntax fix

--- src/main/java/com/cards/cards/api/user/UserController.java
+++ src/main/java/com/cards/cards/api/user/UserController.java
@@ -2,7 +2,6 @@ package com.cards.cards.api.user;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +10,6 @@ import com.cards.cards.models.User;
 import com.cards.cards.services.JwtService;
 import com.cards.cards.services.UserService;
 
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 
 import org.springframework.web.bind.annotation.GetMapping;

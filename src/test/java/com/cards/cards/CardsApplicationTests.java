bb24bf306a59188ac0ee8ac11a60e685dede8395
A252dev <a252p@outlook.com>
2025-05-20 10:39:02 +0300

    unit init

--- src/test/java/com/cards/cards/CardsApplicationTests.java
+++ src/test/java/com/cards/cards/CardsApplicationTests.java
@@ -1,10 +1,19 @@
 package com.cards.cards;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CardsApplicationTests {
+
+	private int value = 1;
+
+	@Test
+	void addition() {
+		assertEquals(2, ++value);
+	}
 
 	@Test
 	void contextLoads() {

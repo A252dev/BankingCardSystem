12b0e4c8cf8ad4c4b47c38021dfe6f450ddbee4a
A252dev <a252p@outlook.com>
2025-05-30 16:32:35 +0300

    account repository

--- src/main/java/com/cards/cards/models/AccountModel.java
+++ src/main/java/com/cards/cards/models/AccountModel.java
@@ -1,10 +1,15 @@
 package com.cards.cards.models;
 
+import java.math.BigDecimal;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -21,5 +26,11 @@ public class AccountModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Integer id;
-    
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
+    private UserModel user_id;
+
+    private BigDecimal balance;
+
 }

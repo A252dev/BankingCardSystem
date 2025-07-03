d9bdf0fdd53b699b4e8b706a964942fa77947746
A252dev <a252p@outlook.com>
2025-07-03 16:23:01 +0300

    redis service & syntax fix

--- src/main/java/com/cards/cards/filters/JwtFilter.java
+++ src/main/java/com/cards/cards/filters/JwtFilter.java
@@ -1,8 +1,14 @@
 package com.cards.cards.filters;
 
 import java.io.IOException;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.StringRedisConnection;
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,6 +17,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.cards.cards.services.JwtService;
+import com.cards.cards.services.RedisService;
 import com.cards.cards.services.UserService;
 
 import jakarta.servlet.FilterChain;
@@ -27,22 +34,29 @@ public class JwtFilter extends OncePerRequestFilter {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private RedisService redisService; // cache service
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
-        String authHeader = request.getHeader("Authorization");
-        String token = null;
-        String name = null;
+        Optional<String> authHeader = Optional.of(request.getHeader("Authorization"));
 
-        if (authHeader != null && authHeader.startsWith("Bearer ")) {
-            token = authHeader.substring(7);
-            name = jwtService.extractName(token);
+        Optional<String> token = Optional.empty();
+        Optional<String> name = Optional.empty();
+
+        if (!authHeader.isEmpty() && authHeader.get().startsWith("Bearer ")) {
+
+            token = Optional.of(authHeader.get().substring(7));
+            name = Optional.of(jwtService.extractName(token.get()));
         }
 
-        if (name != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            UserDetails userDetails = userService.loadUserByUsername(name);
-            if (jwtService.validateToken(token, userDetails)) {
+        if (!name.isEmpty() && SecurityContextHolder.getContext().getAuthentication() == null) {
+
+            UserDetails userDetails = userService.loadUserByUsername(name.get());
+
+            if (jwtService.validateToken(token.get(), userDetails)) {
                 UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails,
                         null, userDetails.getAuthorities());
                 authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
---
+++ src/main/java/com/cards/cards/services/RedisService.java
@@ -0,0 +1,40 @@
+package com.cards.cards.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.StringRedisConnection;
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.RedisOperations;
+
+public class RedisService {
+
+    @Autowired
+    private RedisOperations<String, String> redis;
+
+    public Optional<String> setExData(String key, String value) {
+
+        redis.execute(new RedisCallback<Object>() {
+            public Object doInRedis(RedisConnection connection) throws DataAccessException {
+                if (!((StringRedisConnection) connection).get(key).isEmpty())
+                    return null;
+                else
+                    return ((StringRedisConnection) connection).setEx(key, 7200, value); // 2 hours session
+            }
+        });
+        return null;
+    }
+
+    public Optional<String> setData(String key) {
+
+        redis.execute(new RedisCallback<Object>() {
+            @Override
+            public Object doInRedis(RedisConnection connection) throws DataAccessException {
+                return ((StringRedisConnection) connection).get(key);
+            }
+        });
+        return null;
+    }
+}
